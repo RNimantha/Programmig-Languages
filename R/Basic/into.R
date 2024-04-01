ff540b70427f3b8fe77fcd1e1ec2e50b1455b7ca
Nimantha Bandara <nimantabandara@gmail.com>
2024-04-02 00:04:05 +0530

    add R

---
+++ R/Basic/into.R
@@ -0,0 +1,41 @@
+"Hello World"
+# variables 
+name <- "Nimantha"
+print(name)
+
+# Concatenate Elements
+text <- "awesome"
+paste("R is", text)
+
+text1 <- "R is"
+text2 <- "awesome"
+paste(text1, text2)
+
+#__________________________________________________________
+# Add Variable
+num1 <- 5
+num2 <- 10
+num <- num1 + num2
+print(num)
+
+## Data Types
+
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical/boolean
+x <- TRUE
+class(x)
